d1f14922beb4f259473eb36b2ec1cc13ba983184
khissup <khissup@naver.com>
2022-08-19 17:48:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 636 ms, Memory: 39472 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,71 +1,95 @@
-import java.util.*;
+import java.io.FileInputStream;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Integer>[] list;
-    static int n;
-    static boolean[] check;
+	static int N, M, S;
+	static boolean[] V;
+	static List<ArrayList<Integer>> list;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("input/1260.txt"));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		N =  sc.nextInt();
+		M =  sc.nextInt();
+		S =  sc.nextInt();
+		
+		V = new boolean[N+1];
+		list = new ArrayList<ArrayList<Integer>>();
+		for(int i=0;i<=N;i++) {
+			list.add(new ArrayList<Integer>());
+		}
 
-        list = new ArrayList[n + 1];
+		int from,to;
+		for(int i=0;i<M;i++) { // 간선 M개 들어옴
+			from =  sc.nextInt();
+			to =  sc.nextInt();
+			
+			list.get(from).add(to);
+			list.get(to).add(from);
+		}		
+		for(int i=0;i<=N;i++) {
+			Collections.sort(list.get(i));
+		}
+		
+		V[S] = true;
+		dfs(S);
+		System.out.println();
+		Arrays.fill(V, false);
+		bfs();
+	}
 
-        for (int i = 1; i < n + 1; i++) {
-            list[i] = new ArrayList<Integer>();
-        }
+	private static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+				// Queue<Integer> queue = new ArrayDeque<Integer>();
+		V[S] = true;
+		queue.offer(S);
+		
+		int now;
+		while(!queue.isEmpty()) {
+			now =  queue.poll();
+			System.out.print(now+" ");
+			for( int to :list.get(now)) {
+				if(V[to]) continue;
+				V[to] = true;
+				queue.offer(to);
+			}
+		}
+		
+	}
 
-        for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-
-            list[u].add(v);
-            list[v].add(u);
-        }
-        for (int i = 1; i < n + 1; i++) {
-            Collections.sort(list[i]);
-        }
-        check = new boolean[n + 1];
-        dfs(start);
-        System.out.println();
-
-        check = new boolean[n + 1];
-        bfs(start);
-        System.out.println();
-
-        sc.close();
-    }
-
-    private static void dfs(int x) {
-        if (check[x]) {
-            return;
-        }
-
-        check[x] = true;
-        System.out.print(x + " ");
-        for (int y : list[x]) {
-            if (!check[y])
-                dfs(y);
-        }
-
-    }
-
-    private static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(start);
-        check[start] = true;
-
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-            System.out.print(x + " ");
-            for (int y : list[x]) {
-                if (!check[y]) {
-                    check[y] = true;
-                    queue.add(y);
-                }
-            }
-        }
-    }
+	private static void dfs(int now) {
+		System.out.print(now+" ");
+		
+		for( int to :list.get(now)) {
+			if(V[to]) continue;
+			V[to] = true;
+			dfs(to);
+		}
+		
+	}
+	
+	
+	
+	
 }
+		// 만약 작은값부터 정렬이 안되있을경우
+//		for(int i = 0; i <= N; i++) {
+//			Collections.sort(list.get(i));
+//		}
+/*
+4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4
+*/
